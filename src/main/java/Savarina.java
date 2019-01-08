ef222cd79c45a65191b408072e3c65bf59425214
Grupa 2 Java <seinho_>
2019-01-08 20:52:44 +0200

    import

--- src/main/java/Savarina.java
+++ src/main/java/Savarina.java
@@ -1,17 +1,9 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Savarina {
     private String culoare;
     private String gust;
-
-    public Savarina(String culoare, String gust) {
-        this.culoare = culoare;
-        this.gust = gust;
-    }
-
-    public String getCuloare() {
-        return culoare;
-    }
-
-    public String getGust() {
-        return gust;
-    }
 }
