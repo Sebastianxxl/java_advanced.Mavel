aa2087d101d1025f4974f777d6719ddfed34a1b9
Grupa 2 Java <seinho_>
2019-01-08 20:45:47 +0200

    import

---
+++ src/main/java/Savarina.java
@@ -0,0 +1,17 @@
+public class Savarina {
+    private String culoare;
+    private String gust;
+
+    public Savarina(String culoare, String gust) {
+        this.culoare = culoare;
+        this.gust = gust;
+    }
+
+    public String getCuloare() {
+        return culoare;
+    }
+
+    public String getGust() {
+        return gust;
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Savarina s = new Savarina("alba","dulce");
+
+        System.out.println("Mananc o savarina " + s.getCuloare() + " si " + s.getGust());
+    }
+}
